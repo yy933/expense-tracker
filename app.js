a7540e4d822af8c85d58e83d566bb301fd1fd3a5
yy933 <emilyyu025@gmail.com>
2023-02-15 18:45:37 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require("express-handlebars");
 const mongoose = require('mongoose')
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+
 mongoose.connect(process.env.MONGODB_URI)
 
 // 取得資料庫連線狀態
@@ -17,9 +19,12 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
+app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 // routes
 app.get('/', (req, res, next) => {
-  res.send('Expense tracker')
+  res.render('index')
 })
 app.listen(port, () => {
   console.log(`App is runnung on http://localhost:${port}`)
