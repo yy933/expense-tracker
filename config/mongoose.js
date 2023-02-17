6b2b21f2d79f9a702db3da8899ee6781d93d041b
yy933 <emilyyu025@gmail.com>
2023-02-17 17:20:45 +0800

    refactor: modify mongoose config

---
+++ config/mongoose.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGODB_URI);
+
+// 取得資料庫連線狀態
+const db = mongoose.connection;
+// 連線異常
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+// 連線成功
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+
+module.exports = db
