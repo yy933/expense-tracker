2d96f2883d02c04868b41bf05c7bf26710911c1d
yy933 <emilyyu025@gmail.com>
2023-02-18 15:22:08 +0800

    feat: add message partials

--- config/passport.js
+++ config/passport.js
@@ -7,18 +7,22 @@ module.exports = (app) => {
   app.use(passport.session());
   // 設定本地登入策略
   passport.use(
-    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+    new LocalStrategy({ usernameField: "email", passReqToCallback: true }, (req, email, password, done) => {
       Users.findOne({ email })
         .then((user) => {
           if (!user) {
-            return done(null, false, {
-              message: "That email is not registered!",
-            });
+            return done(
+              null,
+              false,
+              req.flash("warning_msg", "這個Email尚未註冊")
+            );
           }
           if (user.password !== password) {
-            return done(null, false, {
-              message: "Email or Password incorrect.",
-            });
+            return done(
+              null,
+              false,
+              req.flash("warning_msg", "帳號或密碼錯誤!")
+            );
           }
           return done(null, user);
         })
