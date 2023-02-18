12b374ebab908ed104729c2588b8e15791b5494d
yy933 <emilyyu025@gmail.com>
2023-02-18 11:25:54 +0800

    feat: add user login page

--- models/record.js
+++ models/record.js
@@ -25,11 +25,11 @@ const recordSchema = new Schema({
   //   index: true,
   //   required: true,
   // },
-  categoryId: {
-    type: Schema.Types.ObjectId,
-    ref: 'Category',
-    index: true,
-    required: true
-  }
+  // categoryId: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: 'Category',
+  //   index: true,
+  //   required: true
+  // }
 })
 module.exports = mongoose.model('Record', recordSchema)
