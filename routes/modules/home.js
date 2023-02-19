9a51ef2704d6be506155af2296eed38036e8815d
yy933 <emilyyu025@gmail.com>
2023-02-19 09:20:17 +0800

    feat: modify home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,12 +2,19 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/Record')
 
+//首頁
 router.get('/', (req, res, next) => {
   const userId = req.user._id
   Record.find({userId})
     .lean()
     .sort({ date: 'desc' })
-    .then((records) => res.render('index', { records }))
+    .then((records) => {
+      let totalAmount = 0
+      for (let i = 0; i < records.length; i++ ){
+        totalAmount += records[i].amount
+      }
+      return res.render('index', { records, totalAmount })
+    })
     .catch((error) => console.log(error))
 })
 
