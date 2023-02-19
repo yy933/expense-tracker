d79836e9400b139a00047e2af8d0b2706a4c0c63
yy933 <emilyyu025@gmail.com>
2023-02-19 13:27:55 +0800

    feat: modify PUT '/:id' route in records.js

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -59,14 +59,43 @@ router.get('/:id/edit', (req, res, next) => {
     })
 })
 router.put('/:id', (req, res, next) => {
-  const _id = req.params.id
-  const userId = req.user._id
-  const contents = req.body
-  return Record.findOneAndUpdate({ _id, userId }, contents, { new: true })
-
-    .then(() => res.redirect('/'))
+  Category.find()
+    .lean()
+    .then((categories) => {
+      const _id = req.params.id
+      const userId = req.user._id
+      const { itemName, date, categoryId, amount } = req.body
+      const errors = []
+      categories.forEach(category => {
+        if (String(category._id) === categoryId) {
+          category.selected = true
+        } else {
+          category.selected = false
+        }
+      })
+      if (!itemName || !amount || !categoryId || !date) {
+        errors.push({ message: '所有欄位都是必填。' })
+      }
+      if (amount <= 0) {
+        errors.push({ message: '金額必須至少為1元' })
+      }
+      if (errors.length) {
+        return Record.findOne({ _id, userId })
+          .lean()
+          .then(record => {
+            res.render('edit', { errors, record, categories })
+          })
+          .catch(error => console.log(error))
+      }
+      return Record.findOneAndUpdate(
+        { _id, userId },
+        { itemName, date, categoryId, amount, userId },
+        { new: true }
+      ).then(() => res.redirect('/')).catch(error => console.log(error))
+    })
     .catch((error) => console.log(error))
 })
+
 router.delete('/:id', (req, res, next) => {
   const _id = req.params.id
   const userId = req.user._id
